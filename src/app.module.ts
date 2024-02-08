7a5a17a5dd33881fbf02bc0d69be247141cf9ac1
ivan nzima <nzimaivan007@gmail.com>
2024-02-08 18:33:02 +0100

    creating command CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
 import { ServicesModule } from './services/services.module';
+import { CommandsModule } from './commands/commands.module';
 
 @Module({
-  imports: [CustomersModule, ServicesModule],
+  imports: [CustomersModule, ServicesModule, CommandsModule],
   controllers: [AppController],
   providers: [AppService],
 })
